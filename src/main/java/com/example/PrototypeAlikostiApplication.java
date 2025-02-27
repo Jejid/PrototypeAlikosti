a0091179d75901ab21134af9cf16dd44fd598dc3
Edwin <jejidnike@hotmail.com>
2025-02-26 23:09:57 -0500

    initial commit

---
+++ src/main/java/com/example/PrototypeAlikostiApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeAlikostiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeAlikostiApplication.class, args);
+		System.out.println("Hi Riascos");
+	}
+
+}
