be364292c2d3826b84ca94423a9aab29e7f43951
EdwinLopez <jejidnike@hotmail.com>
2025-06-22 15:42:49 -0500

    Paso de validaciones de dao a dto todas las tablas

--- src/main/java/com/example/dao/RequestRefundDao.java
+++ src/main/java/com/example/dao/RequestRefundDao.java
@@ -1,9 +1,6 @@
 package com.example.dao;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/com/example/dto/OrderProcessedDto.java
+++ src/main/java/com/example/dto/OrderProcessedDto.java
@@ -1,6 +1,5 @@
 package com.example.dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
